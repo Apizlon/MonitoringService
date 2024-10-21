dca7b7ddcd16dfbb37334d62b14bfb83a270538c
Dankov Dmitry <Dankov.Dmitry@infotecs-nt>
2024-10-21 17:49:02 +0300

    small comment remarks

--- Source/MonitoringService/MonitoringService.Host/Controllers/ViewController.cs
+++ Source/MonitoringService/MonitoringService.Host/Controllers/ViewController.cs
@@ -13,15 +13,31 @@ namespace MonitoringService.Host.Controllers;
 [EnableCors("AllowAllOrigins")]
 public class ViewController : ControllerBase
 {
+    /// <summary>
+    /// <see cref="StatisticsProcessor"/>
+    /// </summary>
     private readonly StatisticsProcessor _statisticsProcessor;
+    
+    /// <summary>
+    /// Логгер
+    /// </summary>
     private readonly ILogger<ViewController> _logger;
-
+    
+    /// <summary>
+    /// Конструктор контроллера с 2 параметрами
+    /// </summary>
+    /// <param name="statisticsProcessor"><see cref="StatisticsProcessor"/></param>
+    /// <param name="logger">Логгер</param>
     public ViewController(StatisticsProcessor statisticsProcessor,ILogger<ViewController> logger)
     {
         _statisticsProcessor = statisticsProcessor;
         _logger = logger;
     }
     
+    /// <summary>
+    /// Endpoint для получения всех устройств
+    /// </summary>
+    /// <returns>IEnumerable объектов типа <see cref="StatisticsResponse"/></returns>
     [HttpGet]
     public async Task<IActionResult> GetAllStatistics()
     {
