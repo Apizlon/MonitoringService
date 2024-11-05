6ddc284d4a58011bd5d47810577e4579065e1638
Dankov Dmitry <Dankov.Dmitry@infotecs-nt>
2024-11-05 15:51:45 +0300

    Dockerfile added

--- Source/MonitoringService/build.sh
+++ Source/MonitoringService/build.sh
@@ -1,29 +1,39 @@
 #!/bin/bash
 
+#Прекращаем выполнение при ошибке
 set -e
 
-PROJECT_NAME="MonitoringService.Host\MonitoringService.Host.csproj"
-SOLUTION_FILE="MonitoringService.sln"
-OUTPUT_DIR="artifacts"
-
-if ! command -v dotnet &> /dev/null; then
-    echo ".NET SDK не установлен"
-    exit 1
+#Проверка, передан ли аргумент
+if [ -z "$1" ]; then
+echo "Необходимо передать аргумент: test, docs или dev-build"
+exit 1
 fi
 
-echo "Очистка предыдущих сборок..."
-dotnet clean "$SOLUTION_FILE"
+#Установка переменных
+TARGET=$1
+IMAGE_NAME="MonitoringService:$TARGET"
 
-echo "Восстановление зависимостей..."
-dotnet restore "$SOLUTION_FILE"
+#Выполняем сборку на основе переданного аргумента
+case $TARGET in
+    test)
+        echo "Сборка тестового образа..."
+        docker build --target test -t "$IMAGE_NAME" .
+        ;;
+    docs)
+        echo "Сборка образа для генерации документации..."
+        docker build --target docs -t "$IMAGE_NAME" .
+        ;;
+    dev-build)
+        echo "Сборка образа для разработки..."
+        docker build --target dev-build -t "$IMAGE_NAME" .
 
-echo "Сборка проекта..."
-dotnet build "$SOLUTION_FILE" --configuration Debug
+        docker-compose up --build
+        ;;
+    *)
+        echo "Неизвестный тип сборки: $TARGET"
+        echo "Используйте: test, docs или dev-build"
+        exit 1
+        ;;
+esac
 
-echo "Запуск тестов..."
-dotnet test "$SOLUTION_FILE" --configuration Debug
-
-echo "Публикация сборки..."
-dotnet publish "$PROJECT_NAME" --configuration Debug --output "$OUTPUT_DIR"
-
-echo "Сборка завершена."
+echo "Сборка завершена. Образ создан: $IMAGE_NAME"
